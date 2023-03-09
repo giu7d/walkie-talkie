25b906674f690dbc67e8da235e894d07f505a58d
Giuseppe Setem <giuseppe7d@gmail.com>
2023-03-09 08:38:34 -0300

    feat: setup fallback controller

---
+++ apps/api/lib/walkie_talkie_api_web/controllers/fallback_controller.ex
@@ -0,0 +1,17 @@
+defmodule WalkieTalkieApiWeb.FallbackController do
+  use ZeroEightHundredApiWeb, :controller
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(json: HelloWeb.ErrorJSON)
+    |> render(:"404")
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(403)
+    |> put_view(json: HelloWeb.ErrorJSON)
+    |> render(:"403")
+  end
+end
